da886bd1bbb3073ab9c59de5eaaf00b0d58a0abb
danmar669 <141825046+danmar669@users.noreply.github.com>
2024-06-03 22:06:49 +0300

    init project

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+// export default [
+//   {
+//     languageOptions: {
+//       globals: globals.node,
+//     },
+//   },
+//   pluginJs.configs.recommended,
+// ];
+
+export default [
+  pluginJs.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+    },
+  },
+];
